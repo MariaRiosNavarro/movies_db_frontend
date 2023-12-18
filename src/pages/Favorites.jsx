32a1d83161b8d6844b47c7d168e0b2adcc6af149
SuperCoder <propscode254@gmail.com>
2023-12-18 14:40:57 +0100

    fix favs

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -13,7 +13,7 @@ const Favorites = () => {
 
   return (
     <>
-      <section className="px-4">
+      <section className="p-[10rem]">
         <h1>My Favorites</h1>
         <MoviesList array={favorites} arrayType="favorites" />
       </section>
